4370917d3f78574640a4a4ce9c0e2114e7af9468
nileshp98 <44971667+nileshp98@users.noreply.github.com>
2020-03-20 13:20:14 +0530

    Day06 assignments

---
+++ Day06/Nilesh_18311/Bank.java
@@ -0,0 +1,11 @@
+package com.sonata.BankApp;
+
+public interface Bank {
+	
+		
+		public double deposite(double amount);
+		public double withDraw(double amount);
+	}
+
+
+
---
+++ Day06/Nilesh_18311/module-info.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+/**
+ * @author nilesh.patil
+ *
+ */
+module BankModule {
+	requires BankM;
+	exports com.sonata.BankModule1;
+}
